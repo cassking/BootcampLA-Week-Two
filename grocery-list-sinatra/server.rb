e7886f50d0a75b7f1d280534944b22102f9cb3c9
Cassandra King <puddincat@gmail.com>
2018-02-09 12:53:56 -0500

    add sys check for week two, sinatra list with capybara

---
+++ grocery-list-sinatra/server.rb
@@ -0,0 +1,27 @@
+require "sinatra"
+require 'csv'
+require "sinatra/reloader" if development?
+require "pry" if development? || test?
+
+set :bind, '0.0.0.0'  # bind to all interfaces
+
+get '/' do
+  redirect '/grocery-list'
+end
+
+get '/grocery-list' do
+  @groceries = CSV.readlines('grocery_list.csv', headers:true)
+  erb :index
+end
+
+post '/grocery-list' do
+  name = params[:name]
+  if !name.empty?
+    CSV.open("grocery_list.csv", "a") do |csv|
+      csv << [name]
+    end
+  redirect '/grocery-list'
+  else
+    erb :error
+  end
+end
